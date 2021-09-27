1f1db5c8ff03c473a90783e9be5f2bc577ce55a8
ang-jason <angjason896@gmail.com>
2021-09-27 14:30:22 +0800

    base status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './components/header-bar'
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
   return (
-    <div className="App">
-      Hello World j650n
-    </div>
+    <div className="grid place-content-center">
+      <div className="container text-red-500">
+        Hello World j650n
+      
+        <Header/>
+        </div>
+      
+      </div>
   );
 }
 
---
+++ src/components/header-bar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import headerlogo from '../images/headerlogo.jpg'
+
+export default function Header() {
+    return (
+        <div>
+            <img src={headerlogo} alt='breaking bad' />
+        </div>
+    )
+}
